c7702ec17169334d30d29bf4de86bb6eaa857e39
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-21 10:44:47 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -1,26 +1,22 @@
-#include <stdio.h>
-#include <assert.h>
-#include <string.h>
-#include "message.h"
-#include "alerter.h"
 
+int alertFailureCount = 0;
 
-int networkAlertMock(float celcius) {
-    printMsg(celcius);
+int networkAlerter(float celcius) {
+    printMessage(celcius);
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
     return 200;
 }
 
-
-
-int main() {
-    alertInCelcius(400.5,networkAlertMock);
-    alertInCelcius(303.6,networkAlertMock);
-    assert(alertFailureCount==1);
-    assert(alertFailureCount==0);
-    printf("%d alerts failed.\n", alertFailureCount);
-    printf("All is well (maybe!)\n");
-    return 0;
+void alertInCelcius(float farenheit,int (*networkAlert)(float)) {
+    float celcius = (farenheit - 32) * 5 / 9;
+    int returnCode = networkAlert(celcius);
+    if (returnCode != 200) {
+        // non-ok response is not an error! Issues happen in life!
+        // let us keep a count of failures to report
+        // However, this code doesn't count failures!
+        // Add a test below to catch this bug. Alter the stub above, if needed.
+        alertFailureCount += 0;
+    }
 }
